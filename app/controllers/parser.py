d6f78a7596b5139ef7b66ebdaca220f33ddb87ed
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-05-06 13:49:34 +0300

    csx_parser

--- app/controllers/parser.py
+++
@@ -1,9 +0,0 @@
-class BaseParser:
-    def __init__(self):
-        pass
-
-    def get_file(self) -> bool:
-        raise NotImplementedError()
-
-    def parse_data(self, file=None):
-        raise NotImplementedError()
