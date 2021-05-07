8a51d965f0f8680648f1482d2d4ebe573d297d44
tsyboom <tsyboom@gmail.com>
2021-05-07 07:09:21 +0000

    add union link scrapper

--- tests/test_scrapper.py
+++ tests/test_scrapper.py
@@ -1,12 +1,14 @@
 import pytest
-from app.controllers import CSXParser, scrapper
+from app.controllers import CSXParser, scrapper, UnionParser
 from config import BaseConfig as conf
 
 
 @pytest.mark.skipif(not conf.CHROME_DRIVER_PATH, reason="ChromeDriver not configured")
 def test_scrapper():
-    scrapper("csx", 20, 2020, "https://investors.csx.com/metrics/default.aspx")
-    assert scrapper
+    scrap = scrapper("csx", 20, 2020, "https://investors.csx.com/metrics/default.aspx")
+    assert scrap
+    scrap = scrapper("union", 12, 2021, "https://www.up.com/investor/aar-stb_reports/2021_Carloads/index.htm")
+    assert scrap
 
 
 @pytest.mark.skipif(not conf.CHROME_DRIVER_PATH, reason="ChromeDriver not configured")
@@ -16,3 +18,12 @@ def test_csx_scrapper():
     assert csx.file is None
     assert csx.get_file()
     assert csx.file
+
+
+@pytest.mark.skipif(not conf.CHROME_DRIVER_PATH, reason="ChromeDriver not configured")
+def test_union_scraper():
+    union = UnionParser(2021, 15)
+    assert union
+    assert union.file is None
+    assert union.get_file()
+    assert union.file
