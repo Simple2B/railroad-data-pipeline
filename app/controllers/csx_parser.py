d1f218791d913c1b56e81dd542577250089dc96c
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-05-07 10:41:34 +0300

    create data csx for db

--- app/controllers/csx_parser.py
+++ app/controllers/csx_parser.py
@@ -1,10 +1,9 @@
+import re
 from urllib.request import urlopen
 from .base_parser import BaseParser
 import PyPDF2
 from .scrapper import scrapper
 from app.logger import log
-
-# import re
 
 
 class CSXParser(BaseParser):
@@ -26,28 +25,110 @@ class CSXParser(BaseParser):
     def parse_data(self, file=None):
         if not file:
             file = self.file
-        # with open(file, "rb") as pdfFile:
-        pdfReader = PyPDF2.PdfFileReader(file)
-        pages = pdfReader.numPages
-        pages
+        pdf_reader = PyPDF2.PdfFileReader(file)
+        for page_number in range(pdf_reader.numPages):
+            page = pdf_reader.getPage(page_number)
+            pdf_text = page.extractText()
 
-        pg = pdfReader.getPage(0)
-        #print(pg)
-        textPdf = pg.extractText()
-        textPdf
+        format_text = re.sub("\n", " ", pdf_text)
+        text_elem = format_text.split()[12:]
 
-        # pages = pdfReader.getNumPages()
+        def format_list_product(product):
+            list2=[]
+            list3=[]
+            for i in product:
+                if re.search("\d", i):
+                    list3.append(i)
+                else:
+                    list2.append(i)
+            answer = []
+            answer.append(' '.join(list2))
+            answer.extend(list3)
+            return answer
 
-        # body_text = ''
+        list_products_data = []
 
-        # for p in pages:
-        #     page = pdfReader.getPage(p)
-        #     text = page.extractText
+        def create_data_csx(product):
+            dict_product = {}
+            dict_product['product_name'] = product[0]
+            dict_product['week'] = {}
+            dict_product['week']['current_year'] = product[1]
+            dict_product['week']['previous_year'] = product[2]
+            dict_product['week']['сhg'] = product[3]
 
-        #     rex = re.compile("(?<=\%\%\(S\$\))(.*)", re.DOTALL)
-        #     body_text = re.search(rex, text).group(0)
-        #     body_text
+            dict_product['QUARTER_TO_DATE'] = {}
+            dict_product['QUARTER_TO_DATE']['current_year'] = product[4]
+            dict_product['QUARTER_TO_DATE']['previous_year'] = product[5]
+            dict_product['QUARTER_TO_DATE']['сhg'] = product[6]
 
-            # titles = re.findall(r'([a-zA-Z\s])(?=\n)', body)
+            dict_product['YEAR_TO_DATE'] = {}
+            dict_product['YEAR_TO_DATE']['current_year'] = product[7]
+            dict_product['YEAR_TO_DATE']['previous_year'] = product[8]
+            dict_product['YEAR_TO_DATE']['сhg'] = product[9]
 
-            # for title in titles:
+            return list_products_data.append(dict_product)
+
+
+        product_crain = text_elem[:10]
+        product_crain_mill = format_list_product(text_elem[10:22])
+        product_farm = format_list_product(text_elem[22:35])
+        product_food = format_list_product(text_elem[35:46])
+        product_chemicals = text_elem[46:56]
+
+        product_petroleum = format_list_product(text_elem[56:69])
+        product_primary_metal = format_list_product(text_elem[69:81])
+        product_primary_forest = format_list_product(text_elem[81:93])
+        product_lumber = format_list_product(text_elem[93:106])
+        product_pulp = format_list_product(text_elem[106:119])
+        product_non_metallic_minerals = format_list_product(text_elem[119:132])
+        product_crushed_gravel = format_list_product(text_elem[132:146])
+        product_stone_clay_glass = format_list_product(text_elem[146:160])
+        product_iron_steel = format_list_product(text_elem[160:173])
+        product_waste = format_list_product(text_elem[173:186])
+        product_motor = format_list_product(text_elem[186:199])
+        product_metallic = format_list_product(text_elem[199:210])
+        product_coal = text_elem[210:220]
+        product_coke = text_elem[220:230]
+
+        product_all_other_carloads = format_list_product(text_elem[230:242])
+        product_total_carloads = format_list_product(text_elem[242:253])
+        product_trailers = format_list_product(text_elem[253:263])
+        product_containers = format_list_product(text_elem[263:273])
+        product_total_intermodal = format_list_product(text_elem[273:284])
+        product_total_traffic = format_list_product(text_elem[284:295])
+
+        list_products = [
+            product_crain,
+            product_crain_mill,
+            product_farm,
+            product_food,
+            product_chemicals,
+            product_petroleum,
+            product_primary_metal,
+            product_primary_forest,
+            product_lumber,
+            product_pulp,
+            product_non_metallic_minerals,
+            product_crushed_gravel,
+            product_stone_clay_glass,
+            product_iron_steel,
+            product_waste,
+            product_motor,
+            product_metallic,
+            product_coal,
+            product_coke,
+            product_all_other_carloads,
+            product_total_carloads,
+            product_trailers,
+            product_containers,
+            product_total_intermodal,
+            product_total_traffic,
+        ]
+
+        def add_data_products(list_products):
+            for product in list_products:
+                create_data_csx(product)
+
+        add_data_products(list_products)
+
+        return list_products_data
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,8 @@ import pytest
 
 from app import db, create_app
 
-# from app.controllers import CSXParser
+from app.controllers import CSXParser
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CSX_TEST_DATA_FILE = os.path.join(BASE_DIR, 'data/2020-Week-1-AAR.pdf')
@@ -30,7 +31,8 @@ def test_main_page(client):
     assert response.status_code == 200
 
 
-# def test_csx_parser(client):
-#     parser = CSXParser()
-#     with open(CSX_TEST_DATA_FILE, "rb") as file:
-#         parser.parse_data(file=file)
+def test_csx_parser(client):
+    parser = CSXParser(2020, 1)
+    with open(CSX_TEST_DATA_FILE, "rb") as file:
+        filePdf = parser.parse_data(file=file)
+        assert filePdf
