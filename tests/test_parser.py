3e51f31a99507dceae4c451b74f345436e54151e
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-05-21 10:14:41 +0300

    fix test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -52,7 +52,7 @@ def test_union_parser(client):
     COMPANY_ID = "Union_Pacific_2021_2_XX"
     parsed_data = Company.query.filter(Company.company_id == COMPANY_ID).all()
     assert parsed_data
-    # assert len(parsed_data) == 25
+    assert len(parsed_data) == 25
 
 
 def test_kansas_city_southern_parser(client):
