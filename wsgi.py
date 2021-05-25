36ba97eb58a2aa06b59b3bdedb2ee743e206e5a0
tsyboom <tsyboom@gmail.com>
2021-05-25 10:49:03 +0000

    fix kansas

--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,7 @@ from app.logger import log
 
 app = create_app()
 BEGIN_YEAR = int(os.environ.get("BEGIN_YEAR", "2019"))
-CURRENT_YEAR = datetime.datetime.now().year
+CURRENT_YEAR = datetime.datetime.now().year + 1
 CURRENT_WEEK = datetime.datetime.now().date().isocalendar()[1]
 
 
@@ -47,11 +47,11 @@ def scrap():
     """Scrapping all companies"""
     log(log.INFO, "Scrapper started")
     from app.controllers import (
-        CSXParser,
+        # CSXParser,
         # UnionParser,
         # NorfolkSouthernParser,
         # UnionParser,
-        # KansasCitySouthernParser,
+        KansasCitySouthernParser,
         # CanadianNationalParser,
         # CanadianPacificParser,
         # BNSFParser,
@@ -64,10 +64,10 @@ def scrap():
         for week in range(1, finish_week):
             log(log.INFO, "----------------Week %d", week)
             COMPANIES = {
-                CSXParser: "CSX",
+                # CSXParser: "CSX",
                 # UnionParser: "Union Parser",
                 # NorfolkSouthernParser: "Norfolk Southern",
-                # KansasCitySouthernParser = "Kansas City Southern Parser",
+                KansasCitySouthernParser: "Kansas City Southern Parser",
                 # CanadianNationalParser = "Canadian National Parser",
                 # CanadianPacificParser = "CanadianPacificParser",
                 # BNSFParser = "BNSF Parser",
