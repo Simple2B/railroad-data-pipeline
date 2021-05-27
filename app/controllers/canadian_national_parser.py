2692b002cbfcb341cdc48a05cdf63362703e9995
Varvara <varvarashcherbyna7@gmail.com>
2021-05-27 16:04:15 +0000

    fix parser canadian national

--- app/controllers/canadian_national_parser.py
+++ app/controllers/canadian_national_parser.py
@@ -7,7 +7,7 @@ from sqlalchemy import and_
 from urllib.request import urlopen
 from .base_parser import BaseParser
 from app.logger import log
-from .carload_types import find_carload_id
+from .carload_types import find_carload_id, ALL_PROD_TYPES
 from app.models import Company
 
 
@@ -39,26 +39,20 @@ class CanadianNationalParser(BaseParser):
         if not file:
             file = self.file
 
-        if not self.file:
-            log(log.ERROR, "Nothing to parse, file is not found")
-            return None
+        # if not self.file:
+        #     log(log.ERROR, "Nothing to parse, file is not found")
+        #     return None
 
         # Load spreadsheet
         file_xlsx = pd.ExcelFile(file)
         read_xlsx = pd.read_excel(file_xlsx, header=None)
-        xlsx_dicts = read_xlsx.to_dict(orient="dictionary_xlsx")
+        xlsx_dicts = read_xlsx.to_dict("records")
 
-        # xlsx_dicts = read_xlsx.to_dict('dictionary_xlsx')
-        del xlsx_dicts[0]
+        data_dicts = []
 
-        # get the date of report from the general dict
-        months = {}
-        for i, month in enumerate(("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")):
-            months[month] = i + 1
+        xlsx_date = xlsx_dicts.pop(2)[0].replace("-", "")
 
-        date_string = xlsx_dicts[2][10].replace("-", " ")
-
-        dates = search_dates(date_string)
+        dates = search_dates(xlsx_date)
         date = []
 
         for x in dates[0]:
@@ -66,52 +60,32 @@ class CanadianNationalParser(BaseParser):
 
         date = date[1]
 
-        # date = xlsx_dicts[2][10].split(" ")
+        for xlsx_dict in xlsx_dicts:
+            type_name = xlsx_dict[1]
+            if type_name and type_name in ALL_PROD_TYPES:
+                data_dicts.append(xlsx_dict)
 
-        # day = date[-2]
-        # year = date[-1]
-        # month = date[-3]
-        # format_month = months[month]
-
-        # format_day = int(re.findall(r"(\d+)", day)[0])
-        # format_year = int(re.findall(r"(\d+)", year)[0])
-
-        # format_date = datetime(month=format_month, day=format_day, year=format_year)
-
-        xlsx_dicts_types = xlsx_dicts.pop(1)
-
-        # by type of carload value
-        data_dicts = {}
-        for j in xlsx_dicts_types:
-            values_dict = []
-            for index in xlsx_dicts:
-                for i in xlsx_dicts[index]:
-                    if j == i:
-                        values_dict.append(xlsx_dicts[index][i])
-            data_dicts.update({str(xlsx_dicts_types[j]): values_dict})
-
-        # list of all products
         products = {}
 
         for data in data_dicts:
-            if data != "nan":
-                products[data] = dict(
-                    week=dict(
-                        current_year=data_dicts[data][0],
-                        previous_year=data_dicts[data][1],
-                        chg=round(data_dicts[data][3] * 100, 1),
-                    ),
-                    QUARTER_TO_DATE=dict(
-                        current_year=data_dicts[data][5],
-                        previous_year=data_dicts[data][6],
-                        chg=round(data_dicts[data][8] * 100, 1),
-                    ),
-                    YEAR_TO_DATE=dict(
-                        current_year=data_dicts[data][10],
-                        previous_year=data_dicts[data][11],
-                        chg=round(data_dicts[data][13] * 100, 1),
-                    ),
-                )
+            products[data[1]] = dict(
+                week=dict(
+                    current_year=data[2],
+                    previous_year=data[3],
+                    chg=round(data[5] * 100, 1),
+                ),
+                QUARTER_TO_DATE=dict(
+                    current_year=data[7],
+                    previous_year=data[8],
+                    chg=round(data[10] * 100, 1),
+                ),
+                YEAR_TO_DATE=dict(
+                    current_year=data[12],
+                    previous_year=data[13],
+                    chg=round(data[15] * 100, 1),
+                ),
+            )
+
         # write data to the database
         for prod_name, product in products.items():
             company_id = ""
@@ -135,7 +109,7 @@ class CanadianNationalParser(BaseParser):
                     YTDCarloads=products[prod_name]["YEAR_TO_DATE"]["current_year"],
                     YOYYDCarloads=products[prod_name]["YEAR_TO_DATE"]["current_year"]
                     - products[prod_name]["YEAR_TO_DATE"]["previous_year"],
-                    date=date[1],
+                    date=date,
                     week=self.week_no,
                     year=self.year_no,
                     company_name="Canadian National",
